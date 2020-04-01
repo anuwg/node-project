1f611f86238ae1b4c9d0e0af00ee2492af44bf2f
anuwg <anuwg2019@gmail.com>
2020-04-02 00:03:09 +1100

    Set up app for heroku

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ console.log(__dirname)
 console.log(path.join(__dirname,'../public'))
 
 const app= express()
+const port = process.env.PORT|| 3000
 //defines paths for express config
 const publicDirectory = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -100,6 +101,6 @@ app.get('*', (req, res)=>{
 //app.com/help
 //app.com/about
 //app.com/weather
-app.listen(3000, ()=>{
-    console.log('server is up on port 3000')
+app.listen(port, ()=>{
+    console.log('server is up on port ' + port)
 })
